423f1b02363faf1d10c86342c4381460dace70a9
Ananya Gautam <ananya12mar@gmail.com>
2026-08-10 23:57:33 +0530

    Max frequency element

--- 1_Math.cpp
+++ 1_Math.cpp
@@ -193,6 +193,54 @@ int mostFrequentElement(vector<int> &nums)
     return maxnum;
 }
 
+int maxFrequency(vector<int> &nums, int k)
+{
+    sort(nums.begin(), nums.end());
+
+    int low = 0;
+    int high = 1;
+    int ans = 1;
+
+    while (low <= high && high < nums.size())
+    {
+
+        if (nums[high] == nums[high - 1])
+        {
+            ans = max(ans, high - low + 1);
+            high++;
+        }
+
+        else if (nums[high] > nums[high - 1] && k > 0)
+        {
+
+            long long val = nums[high] - nums[high - 1];
+            long long need = val * (high - low);
+
+            if (need <= k)
+            {
+                k -= need;
+                high++;
+                ans = max(ans, high - low);
+            }
+
+            else
+            {
+                k += nums[high - 1] - nums[low];
+                low++;
+            }
+        }
+
+        else
+        {
+
+            k += nums[high - 1] - nums[low];
+            low++;
+        }
+    }
+
+    return ans;
+}
+
 vector<int> selectionSort(vector<int> &nums)
 {
     for (int i = 0; i < nums.size(); i++)
