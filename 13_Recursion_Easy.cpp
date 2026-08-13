4fc3252b2ae5fe62d2ce8f00d587881f6b8e3bc5
Ananya Gautam <ananya12mar@gmail.com>
2026-08-13 23:48:44 +0530

    Count Good Numbers

--- 13_Recursion_Easy.cpp
+++ 13_Recursion_Easy.cpp
@@ -59,10 +59,37 @@ double myPow(double x, long long n)
     return power(x, n);
 }
 
+long long helper(long long base, long long expo, long long mod)
+{
+    long long ans = 1;
+    while (expo > 0)
+    {
+        if (expo % 2 == 0)
+        {
+            base = (base * base) % mod;
+            expo = expo / 2;
+        }
+        else
+        {
+            ans = (ans * base) % mod;
+            expo -= 1;
+        }
+    }
+    return ans;
+}
+
+int countGoodNumbers(long long n)
+{
+    long long even = (n + 1) / 2;
+    long long odd = n / 2;
+    long long mod = 1e9 + 7;
+    return (int)((helper(5, even, mod) * helper(4, odd, mod)) % mod);
+}
+
 int main()
 {
-    double x;
+    // double x;
     long long n;
-    cin >> x >> n;
-    cout << myPow(x, n);
+    cin >> n;
+    cout << countGoodNumbers(n);
 }
