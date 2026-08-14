67274914c9bc7ab3ece597885d66fba170ef016d
Ananya Gautam <ananya12mar@gmail.com>
2026-08-14 23:50:54 +0530

    reverse a stack

--- 13_Recursion_Easy.cpp
+++ 13_Recursion_Easy.cpp
@@ -114,6 +114,29 @@ void sortStack(stack<int> &s)
     }
 }
 
+void insertAtBottom(stack<int> &st, int val)
+{
+    if (st.empty())
+    {
+        st.push(val);
+        return;
+    }
+    int topVal = st.top();
+    st.pop();
+    insertAtBottom(st, val);
+    st.push(topVal);
+}
+
+void reverseStack(stack<int> &st)
+{
+    if (st.empty())
+        return;
+    int topVal = st.top();
+    st.pop();
+    reverseStack(st);
+    insertAtBottom(st, topVal);
+}
+
 int main()
 {
     // double x;
