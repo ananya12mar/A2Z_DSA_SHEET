15eb8edb8ae775282bf7b378992844a71b0f279e
Ananya Gautam <ananya12mar@gmail.com>
2026-08-19 00:43:30 +0530

    minBitFlip BM

---
+++ 17_Bit_Manipulation_Medium.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minBitsFlip(int start, int goal)
+{
+    int x = start ^ goal;
+    int ans = 0;
+    while (x > 0)
+    {
+        if (x & 1)
+            ans++;
+        x = x >> 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    // int n, m;
+    // cin >> n;
+    // vector<int> nums(n);
+    // for (int i = 0; i < n; i++)
+    //     cin >> nums[i];
+    // vector<int> ans = singleNumberIII(nums);
+    // for (auto v : ans)
+    //     cout << v << " ";
+}
