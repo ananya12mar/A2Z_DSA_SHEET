4c2464a307b190c9bcb31d8b578b58e97fb8af71
Ananya Gautam <ananya12mar@gmail.com>
2026-08-19 00:42:32 +0530

    divide BM

--- 16_Bit_Manipulation_Easy.cpp
+++ 16_Bit_Manipulation_Easy.cpp
@@ -45,10 +45,75 @@ void swap(int &a, int &b)
     a = a ^ b;
 }
 
+int divide(int dividend, int divisor)
+{
+    if (dividend == divisor)
+        return 1;
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+    if (divisor == 1)
+        return dividend;
+
+    bool isPositive = true;
+    if (dividend >= 0 && divisor < 0)
+        isPositive = false;
+    else if (dividend < 0 && divisor > 0)
+        isPositive = false;
+
+    long long n = dividend;
+    long long d = divisor;
+
+    n = abs(n);
+    d = abs(d);
+    long long ans = 0, sum = 0;
+
+    while (sum + d <= n)
+    {
+        ans++;
+        sum += d;
+    }
+
+    if (ans > INT_MAX && isPositive)
+        return INT_MAX;
+    if (ans > INT_MAX && !isPositive)
+        return INT_MIN;
+
+    return isPositive ? ans : -ans;
+}
+
+int divideBM(int dividend, int divisor)
+{
+    if (dividend == divisor)
+        return 1;
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+    if (divisor == 1)
+        return dividend;
+    bool isPositive = (dividend < 0 == divisor < 0);
+
+    long long a = dividend;
+    long long b = divisor;
+    a = abs(a);
+    b = abs(b);
+    unsigned int ans = 0;
+
+    while (a >= b)
+    {
+        short q = 0;
+        while (a > (b << (q + 1)))
+            q++;
+        ans += (1 << q);
+        a = a - (b << q);
+    }
+    if (ans == (1 << 31) and isPositive)
+        return INT_MAX;
+    return isPositive ? ans : -ans;
+}
+
 int main()
 {
     int n, i;
     cin >> n >> i;
-    swap(n, i);
-    cout << n << " " << i;
+    int ans = divideBM(n, i);
+    cout << ans;
 }
