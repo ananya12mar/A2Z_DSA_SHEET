d5d4b0fbd1b57b78840cd2c94e7e4b8162bf787b
Ananya Gautam <ananya12mar@gmail.com>
2026-08-19 15:32:11 +0530

    primeFactors BM

---
+++ 18_Bit_Manipulation_Hard.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+vector<vector<int>> primeFactors(vector<int> &queries)
+{
+    vector<vector<int>> result;
+    for (auto v : queries)
+    {
+        vector<int> factors;
+        for (int i = 2; i * i <= v; i++)
+        {
+            if (v % i == 0)
+            {
+                while (v % i == 0)
+                {
+                    if (isPrime(i))
+                        factors.push_back(i);
+                    v = v / i;
+                }
+            }
+        }
+        if (v != 1)
+            factors.push_back(v);
+        result.push_back(factors);
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    // cout << isPrime(n) << endl;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    vector<vector<int>> ans = primeFactors(nums);
+    for (auto v : ans)
+    {
+        for (auto e : v)
+            cout << e << " ";
+        cout << "\n";
+    }
+}
