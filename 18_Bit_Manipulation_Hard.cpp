f112b7b9dfa28d37d981b323b3e15e59d9346bec
Ananya Gautam <ananya12mar@gmail.com>
2026-08-20 09:56:49 +0530

    primesInRange Math

--- 18_Bit_Manipulation_Hard.cpp
+++ 18_Bit_Manipulation_Hard.cpp
@@ -53,15 +53,64 @@ vector<int> divisors(int n)
     return factors;
 }
 
+vector<int> primesInRange(vector<vector<int>> &queries)
+{
+    if (queries.empty())
+        return {};
+
+    // Find the maximum value(second value) in the queries to determine the sieve range
+    int maxVal = 0;
+    for (auto query : queries)
+        maxVal = max(maxVal, query[1]);
+
+    // Use the Sieve of Eratosthenes to find all primes up to maxVal
+    vector<bool> isPrime(maxVal + 1, true);
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not primes
+    for (int p = 2; p * p <= maxVal; ++p)
+    {
+        if (isPrime[p])
+        {
+            for (int i = p * p; i <= maxVal; i += p)
+                isPrime[i] = false;
+        }
+    }
+
+    // Create a prefix sum array to count primes up to each number
+    vector<int> primeCount(maxVal + 1, 0);
+    for (int i = 1; i <= maxVal; ++i)
+    {
+        primeCount[i] = primeCount[i - 1];
+        if (isPrime[i])
+        {
+            primeCount[i]++;
+        }
+    }
+
+    // Process each query to find the number of primes in the given range
+    vector<int> result;
+    for (auto query : queries)
+    {
+        int start = query[0];
+        int end = query[1];
+        if (start == 0)
+            result.push_back(primeCount[end]);
+        else
+            result.push_back(primeCount[end] - primeCount[start - 1]);
+    }
+    return result;
+}
+
 int main()
 {
     int n;
     cin >> n;
     // cout << isPrime(n) << endl;
-    // vector<int> nums(n);
-    // for (int i = 0; i < n; i++)
-    //     cin >> nums[i];
-    vector<int> ans = divisors(n);
+    vector<vector<int>> nums;
+    nums.assign(n, vector<int>(2));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> nums[i][j];
+    vector<int> ans = primesInRange(nums);
     for (auto v : ans)
     {
         cout << v << " ";
