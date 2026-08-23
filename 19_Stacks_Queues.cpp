7995d56d6fda2cd6d360aa643849492c11fcea68
Ananya Gautam <ananya12mar@gmail.com>
2026-08-23 13:54:57 +0530

    ArrayStack

---
+++ 19_Stacks_Queues.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ArrayStack
+{
+private:
+    int *stackArray;
+    int capacity;
+    int topIndex;
+
+public:
+    ArrayStack(int size = 1000)
+    {
+        capacity = size;
+        stackArray = new int[capacity];
+        // Initialize stack as empty
+        topIndex = -1;
+    }
+    // Destructor
+    ~ArrayStack()
+    {
+        delete[] stackArray;
+    }
+    // Pushes element x
+    void push(int x)
+    {
+        if (topIndex >= capacity - 1)
+        {
+            cout << "Stack overflow" << endl;
+            return;
+        }
+        stackArray[++topIndex] = x;
+    }
+    // Removes and returns top element
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+            // Return invalid value
+            return -1;
+        }
+        return stackArray[topIndex--];
+    }
+    // Returns top element
+    int top()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return stackArray[topIndex];
+    }
+    /* Returns true if the stack is empty, false otherwise*/
+    bool isEmpty()
+    {
+        return topIndex == -1;
+    }
+};
+
+int main()
+{
+    ArrayStack stack;
+    vector<string> commands = {"ArrayStack", "push", "push", "top", "pop", "isEmpty"};
+    vector<vector<int>> inputs = {{}, {5}, {10}, {}, {}, {}};
+
+    for (size_t i = 0; i < commands.size(); ++i)
+    {
+        if (commands[i] == "push")
+        {
+            stack.push(inputs[i][0]);
+            cout << "null ";
+        }
+        else if (commands[i] == "pop")
+        {
+            cout << stack.pop() << " ";
+        }
+        else if (commands[i] == "top")
+        {
+            cout << stack.top() << " ";
+        }
+        else if (commands[i] == "isEmpty")
+        {
+            cout << (stack.isEmpty() ? "true" : "false") << " ";
+        }
+        else if (commands[i] == "ArrayStack")
+        {
+            cout << "null ";
+        }
+    }
+
+    return 0;
+}
