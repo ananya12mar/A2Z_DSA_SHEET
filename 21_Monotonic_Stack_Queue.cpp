00f2c7ed1cb9d38984769fa2e0f6e44c0074d585
Ananya Gautam <ananya12mar@gmail.com>
2026-08-25 00:57:55 +0530

    nextLargerElement shorter code

--- 21_Monotonic_Stack_Queue.cpp
+++ 21_Monotonic_Stack_Queue.cpp
@@ -7,31 +7,15 @@ vector<int> nextLargerElement(vector<int> arr)
     vector<int> ans;
     for (int i = arr.size() - 1; i >= 0; i--)
     {
+        while (!st.empty() && st.top() < arr[i])
+            st.pop();
+
         if (st.empty())
-        {
             ans.push_back(-1);
-            st.push(arr[i]);
-        }
         else
-        {
-            if (st.top() < arr[i])
-            {
-                while (!st.empty() && st.top() < arr[i])
-                    st.pop();
+            ans.push_back(st.top());
 
-                if (st.empty())
-                    ans.push_back(-1);
-                else
-                    ans.push_back(st.top());
-
-                st.push(arr[i]);
-            }
-            else
-            {
-                ans.push_back(st.top());
-                st.push(arr[i]);
-            }
-        }
+        st.push(arr[i]);
     }
     reverse(ans.begin(), ans.end());
     return ans;
