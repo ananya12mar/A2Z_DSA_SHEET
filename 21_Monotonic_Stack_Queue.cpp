38f2fc7f7f0239d4826d733dfad1f14a25467d54
Ananya Gautam <ananya12mar@gmail.com>
2026-08-25 00:33:56 +0530

    nextGreaterElement-leetcode

--- 21_Monotonic_Stack_Queue.cpp
+++ 21_Monotonic_Stack_Queue.cpp
@@ -37,6 +37,42 @@ vector<int> nextLargerElement(vector<int> arr)
     return ans;
 }
 
+vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+{
+    vector<int> ans = nextLargerElement(nums2);
+    map<int, int> mp;
+    vector<int> res;
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        mp.insert({nums2[i], ans[i]});
+    }
+    for (auto a : nums1)
+    {
+        res.push_back(mp[a]);
+    }
+    return res;
+}
+
 int main()
 {
+    int n, m;
+    cin >> n >> m;
+    vector<int> v1, v2;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v1.push_back(x);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        v2.push_back(x);
+    }
+    vector<int> ans = nextGreaterElement(v1, v2);
+    for (auto c : ans)
+    {
+        cout << c << " ";
+    }
 }
