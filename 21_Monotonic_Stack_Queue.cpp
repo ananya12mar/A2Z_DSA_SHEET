eed4b6d3abd57f49e2e6270079fe1c71b095da36
Ananya Gautam <ananya12mar@gmail.com>
2026-08-26 23:45:55 +0530

    asteroid Collection

--- 21_Monotonic_Stack_Queue.cpp
+++ 21_Monotonic_Stack_Queue.cpp
@@ -202,6 +202,37 @@ int sumSubarrayMins(vector<int> &arr)
     return sum;
 }
 
+vector<int> asteroidCollision(vector<int> &asteroids)
+{
+    vector<int> ans;
+    vector<int> st;
+    for (int i = 0; i < asteroids.size(); i++)
+    {
+        if (asteroids[i] > 0)
+        {
+            st.push_back(asteroids[i]);
+        }
+        else
+        {
+            while (!st.empty() && st.back() > 0 &&
+                   st.back() < abs(asteroids[i]))
+            {
+                st.pop_back();
+            }
+            if (!st.empty() &&
+                st.back() == abs(asteroids[i]))
+            {
+                st.pop_back();
+            }
+            else if (st.empty() ||
+                     st.back() < 0)
+            {
+                st.push_back(asteroids[i]);
+            }
+        }
+    }
+    return st;
+}
 int main()
 {
     int n;
