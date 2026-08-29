9cd5838f6fe5bfa2a72f190f8fbfe92fd991a163
Ananya Gautam <ananya12mar@gmail.com>
2026-08-30 02:23:03 +0530

    celebrityProblem

--- 22_implementation_problems.cpp
+++ 22_implementation_problems.cpp
@@ -56,6 +56,42 @@ vector<int> stockSpan(vector<int> arr, int n)
     return ans;
 }
 
+int celebrity(vector<vector<int>> &M)
+{
+    int n = M.size();
+    int m = M[0].size();
+    int top = 0;
+    int down = n - 1;
+
+    while (top < down)
+    {
+        if (M[top][down] == 1)
+            top++;
+        else if (M[down][top] == 1)
+            down--;
+        else
+        {
+            top++;
+            down--;
+        }
+    }
+    if (top > down)
+        return -1;
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (i == top)
+                continue;
+            if (M[top][i] == 0 && M[i][top] == 1)
+                continue;
+            else
+                return -1;
+        }
+    }
+    return top;
+}
+
 int main()
 {
     int n, k;
